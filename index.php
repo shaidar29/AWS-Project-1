5266f3e14504106888c7e3731af3a9c60d3f1032
shaidar29 <153365495+shaidar29@users.noreply.github.com>
2023-12-28 17:47:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ background-size: 100% 100%">
 <?php
 $firstname=$_POST['firstname'];
 $email=$_POST['email'];
-$servername = "database-1.cjqjwfrtsgii.us-east-1.rds.amazonaws.com";
+$servername = "sdb-db.cjqjwfrtsgii.us-east-1.rds.amazonaws.com";
 $username = "admin";
 $password = "intel123";
 $db = "intel";
